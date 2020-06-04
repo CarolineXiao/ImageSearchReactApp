37aa53fbe7a7f134678cbcda3628fd905aef0ce7
Jiayue Xiao <j32xiao@uwaterloo.ca>
2020-06-03 22:11:37 -0400

    added server

---
+++ build/precache-manifest.c60fe84e11bdaf95ea3036701bb033d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16a44a107a8b92bf57b1bc0810b1c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4887c2aea2d86c8d636",
+    "url": "/static/css/main.465765bc.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4887c2aea2d86c8d636",
+    "url": "/static/js/main.4fb5e941.chunk.js"
+  },
+  {
+    "revision": "46525b4c7b15e71e68ae",
+    "url": "/static/js/runtime-main.5fd334c5.js"
+  }
+]);
